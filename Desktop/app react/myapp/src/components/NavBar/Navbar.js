1979344dada2e073e94adb59deaff1675c3e1640
Misael <misa132456@gmail.com>
2022-11-22 21:49:24 -0300

    jsx

---
+++ Desktop/app react/myapp/src/components/NavBar/Navbar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>MyApp</h1>
+            <button>"boton1</button>
+            <button className="boton">boton 2</button>
+        </nav>
+    )
+}
+
+export default Navbar
